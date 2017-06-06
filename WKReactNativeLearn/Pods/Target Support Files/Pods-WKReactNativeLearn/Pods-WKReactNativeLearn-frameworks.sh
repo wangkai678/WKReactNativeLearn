1f282ad53046a3203c0c8f576ee5d7d5d96750b1
王凯 <kai.wang@srgroup.cn>
2017-06-06 09:33:29 +0800

    安装node

--- WKReactNativeLearn/Pods/Target Support Files/Pods-WKReactNativeLearn/Pods-WKReactNativeLearn-frameworks.sh
+++ WKReactNativeLearn/Pods/Target Support Files/Pods-WKReactNativeLearn/Pods-WKReactNativeLearn-frameworks.sh
@@ -82,3 +82,12 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/React/React.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Yoga/yoga.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/React/React.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Yoga/yoga.framework"
+fi
--- WKReactNativeLearn/WKReactNativeLearn/ViewController.m
+++ WKReactNativeLearn/WKReactNativeLearn/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-//#import <React/RCTRootView.h>
+#import <React/RCTRootView.h>
 
 @interface ViewController ()
 
@@ -26,28 +26,28 @@
 }
 
 - (IBAction)highScores:(UIButton *)sender {
-//    NSURL *jsCodeLocation = [NSURL
-//                             URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios"];
-//    RCTRootView *rootView =
-//    [[RCTRootView alloc] initWithBundleURL : jsCodeLocation
-//                         moduleName        : @"RNHighScores"
-//                         initialProperties :
-//     @{
-//       @"scores" : @[
-//               @{
-//                   @"name" : @"Alex",
-//                   @"value": @"42"
-//                   },
-//               @{
-//                   @"name" : @"Joel",
-//                   @"value": @"10"
-//                   }
-//               ]
-//       }
-//                          launchOptions    : nil];
-//    UIViewController *vc = [[UIViewController alloc] init];
-//    vc.view = rootView;
-//    [self presentViewController:vc animated:YES completion:nil];
+    NSURL *jsCodeLocation = [NSURL
+                             URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios"];
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL : jsCodeLocation
+                         moduleName        : @"RNHighScores"
+                         initialProperties :
+     @{
+       @"scores" : @[
+               @{
+                   @"name" : @"Alex",
+                   @"value": @"42"
+                   },
+               @{
+                   @"name" : @"Joel",
+                   @"value": @"10"
+                   }
+               ]
+       }
+                          launchOptions    : nil];
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    [self presentViewController:vc animated:YES completion:nil];
 }
 
 
